7f2b822881ddcc082ba6f21e8d0d8fd35aeb0009
Nubzzin <matheus.thelas@hotmail.com>
2023-04-12 23:50:02 -0300

    Add categorias de palavras

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -7,6 +7,10 @@ def jogar():
     print('*******Jogo da forca*********')
     print('*****************************')
 
+    # Ler arquivo
+    palavras = read_csv('palavras_adivinha.csv')
+    coluna = randrange(4)
+
     # Escolhendo dificuldade
     def escolha_dificuldade():
         dificuldade = input('Olá escolha uma dificuldade:\n0-Facil(15 chances) / 1-Medio(10 chances) / 2-Dificil(5 chances)\n:')
@@ -18,7 +22,7 @@ def jogar():
         elif dificuldade == '2':
             chances = 5
         else:
-            print('Error, numero invalido')
+            print('Error, opção invalida')
             escolha_dificuldade()
 
         return chances
@@ -26,12 +30,18 @@ def jogar():
     chances = escolha_dificuldade()
 
     # Coleta palavras e as normaliza
-    def gerar_palavras_secretas():
-        palavras = read_csv('palavras_adivinha.csv')
+    def gerar_palavras_secretas_e_categoria(palavras,coluna):
         palavras_escolhidas = []
-        for palavra in palavras.palavras_adivinha:
+
+        # Gerar categoria
+        
+        categoria = palavras.iloc[:,coluna]
+
+        # Gerar palavra
+        for palavra in categoria:
             palavras_escolhidas.append(palavra)
         return palavras_escolhidas
+    
 
     # Gera uma palavra aleatoria para ser adivinhada
     def sorteiar_palavra(palavras_escolhidas):
@@ -40,13 +50,14 @@ def jogar():
         return palavras_sorteada.lower()
 
     # Verifica se o chute esta certo
-    def verificar_chute(palavra_sorteada,chances):
+    def verificar_chute(palavra_sorteada,chances,palavras,coluna):
         posicoes = ['_' for i in range(len(palavra_sorteada))]
         print(posicoes, len(posicoes))
         
         # Enquanto chances maior que 0
         while(chances>0):
             print(f'Você tem {chances} chances')
+            print(f'A categoria é {palavras.columns[coluna]}')
             chute = input('Manda a letra pro pai: ').lower()
             chutes = list(chute)
 
@@ -72,7 +83,7 @@ def jogar():
             elif chances == 0:
                 print(f'Mongão, chances acabaram\n A palavra era: {palavra_sorteada.title()}')
             
-    verificar_chute(sorteiar_palavra(gerar_palavras_secretas()),chances)
+    verificar_chute(sorteiar_palavra(gerar_palavras_secretas_e_categoria(palavras,coluna)),chances,palavras,coluna)
 
 if __name__ == '__main__':
     jogar()
